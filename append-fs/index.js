8b16fddad54c545b20049de54fd4ebbd6790b8d6
OscarPantoja <oskar.pantoja@gmail.com>
2020-01-28 19:23:50 -0600

    [MOOD]se agrego comentario de funcion

--- append-fs/index.js
+++ append-fs/index.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+//como primer parametro la ruta del archivo que se va a copiar, como segundo el destino
+// y por ultimo el callback
+
 fs.appendFile('/home/oscarp/kodemia/nodejs/nodejs-class/write-fs/prueba2.txt', ', y No entiende!!', (err) => {
     if (err) throw err;
     console.log('Archivo actualizado');
