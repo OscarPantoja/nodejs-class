4df98873321150095d5e8494c36352a2c2b8e9bf
OscarPantoja <oskar.pantoja@gmail.com>
2020-01-28 18:25:35 -0600

    [MOOD]readFile 'fs'

--- read-fs/index.js
+++ read-fs/index.js
@@ -1,1 +1,10 @@
 const fs = require('fs');
+
+//readFile es para leer el contenido de un archivo
+//como primer argumento la ubicación del archivo, el formato de codificación
+//y el callback
+
+fs.readFile('/home/oscarp/kodemia/nodejs/nodejs-class/write-fs/prueba2.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
---
+++ write-fs/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+
+//writeFile recibe el nombre del archivo a crear y la ruta donde se va a crear, 
+//el contenido del archivo y el callback para saber si hay un error
+
+fs.writeFile('./prueba2.txt', 'texto de prueba para que Itiel entienda', function (err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Archivo creado')
+});
